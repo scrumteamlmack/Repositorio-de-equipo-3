9f5b17bca9fc54f54384840ed825eb8dc27f4f05
D4vidCz <dscruzamado111@gmail.com>
2025-06-17 16:31:59 -0500

    file: se agregan nuevas funciones a mysql

---
+++ Bases de datos/script BD/funciones.sql
@@ -0,0 +1,36 @@
+-- Función 1: obtener estado ambiente
+DELIMITER //
+CREATE FUNCTION estado_ambiente(id INT)
+RETURNS VARCHAR(30)
+READS SQL DATA
+BEGIN
+    DECLARE estado_actual VARCHAR(30);
+    SELECT estado INTO estado_actual FROM ambiente WHERE id_ambiente = id;
+    RETURN estado_actual;
+END //
+DELIMITER ;
+
+-- Función 2: verificar recursos por ambiente
+DELIMITER //
+CREATE FUNCTION total_recursos_ambiente(id INT)
+RETURNS INT
+READS SQL DATA
+BEGIN
+    DECLARE total INT;
+    SELECT COUNT(*) INTO total FROM recursos WHERE ambiente_id = id;
+    RETURN total;
+END //
+DELIMITER ;
+
+-- Función 3: obtener incidentes por ambiente
+DELIMITER //
+CREATE FUNCTION contar_incidentes_ambiente(id INT)
+RETURNS INT
+READS SQL DATA
+BEGIN
+    DECLARE total INT;
+    SELECT COUNT(*) INTO total FROM registro_incidente WHERE ambiente_id = id;
+    RETURN total;
+END //
+DELIMITER ;
+
--- Bases de datos/script BD/procedimientos.sql
+++ Bases de datos/script BD/procedimientos.sql
@@ -46,4 +46,66 @@ BEGIN
 END //
 DELIMITER ;
 
+-- Procedimiento 4: asignar recursos a ambiente
+DELIMITER //
+CREATE PROCEDURE asignar_recurso_ambiente(
+    IN serial VARCHAR(30),
+    IN numero TINYINT,
+    IN estado VARCHAR(20),
+    IN observacion TEXT,
+    IN ambiente INT,
+    IN tipo INT
+)
+BEGIN
+    INSERT INTO recursos(serial_recurso, num_recurso, estado, observacion, ambiente_id, recurso_id)
+    VALUES (serial, numero, estado, observacion, ambiente, tipo);
+END //
+DELIMITER ;
+
+-- Procedimiento 5: actualizar estado recurso
+DELIMITER //
+CREATE PROCEDURE actualizar_estado_recurso(
+    IN serial VARCHAR(30),
+    IN nuevo_estado VARCHAR(20)
+)
+BEGIN
+    UPDATE recursos
+    SET estado = nuevo_estado
+    WHERE serial_recurso = serial;
+END //
+DELIMITER ;
+
+-- Procedimiento 6: mostrar ambientes disponibles
+DELIMITER //
+CREATE PROCEDURE ambientes_disponibles()
+BEGIN
+    SELECT * FROM ambiente WHERE estado = 'disponible';
+END //
+DELIMITER ;
+
+-- Procedimiento 7: cambiar responsable minuta
+DELIMITER //
+CREATE PROCEDURE actualizar_responsable_minuta(
+    IN id_minuta INT,
+    IN nuevo_responsable VARCHAR(30)
+)
+BEGIN
+    UPDATE registro_minuta
+    SET responsable = nuevo_responsable
+    WHERE id_minuta = id_minuta;
+END //
+DELIMITER ;
+
+-- Procedimiento 8: reporte de recursos de ambiente
+DELIMITER //
+CREATE PROCEDURE reporte_recursos_ambiente(
+    IN id_ambiente INT
+)
+BEGIN
+    SELECT r.serial_recurso, tr.recurso_tipo, r.estado, r.observacion
+    FROM recursos r
+    JOIN tipo_recurso tr ON r.recurso_id = tr.id_recurso
+    WHERE r.ambiente_id = id_ambiente;
+END //
+DELIMITER ;
 
