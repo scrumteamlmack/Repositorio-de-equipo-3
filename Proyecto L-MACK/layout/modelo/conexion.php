3125dd0ed4358ee3a7d03cf8fdc67fbbe640fa85
Kennencortes <kennencortes123@gmail.com>
2025-09-08 20:37:06 -0500

    Docs:correccion de errores de a ceurdo a sonarqube (seguridad)

--- Proyecto L-MACK/layout/modelo/conexion.php
+++ Proyecto L-MACK/layout/modelo/conexion.php
@@ -5,13 +5,22 @@ class Conexion{
     private $host = "localhost";
     private $db = "mydb";
     private $user = "root";
-    private $password = "";
+    private $password;
+
+    public function __construct(){
+        // Obtener la contraseña desde la variable de entorno
+        $this->password = getenv("MYSQL_SECURE_PASSWORD");
+    }
 
     public static function getConexion(){
 $conexion= null;
 try{
+   // Crear instancia de la clase para acceder a las propiedades
+            $instancia = new self();
    // $conexion = new PDO("mysql:host=".$host.";dbname=$db", $user, $password);
-   $conexion = new PDO("mysql:host=localhost;dbname=mydb", "root", "");
+   $conexion = new PDO("mysql:host=" . $instancia->host . ";dbname=" . $instancia->db,
+                $instancia->user,
+                $instancia->password);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 //echo "Conexion exitosa a la base de datos.";
 } catch(PDOException $e){
