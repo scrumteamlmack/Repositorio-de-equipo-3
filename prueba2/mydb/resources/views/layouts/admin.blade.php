be813b19431bbad972ce6319fd5e9509f351d95b
Mahilynwn <mahilyg17@gmail.com>
2025-09-24 18:22:23 -0500

    panel_admin.php

---
+++ prueba2/mydb/resources/views/layouts/admin.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel Administrador</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+
+  
+    <script>
+        function toggleSubmenu(element) {
+            let submenu = element.querySelector(".submenu");
+            let visible = submenu.style.display === "block";
+            // Cerrar todos los submenús
+            document.querySelectorAll(".submenu").forEach(s => s.style.display = "none");
+            // Abrir solo el actual
+            submenu.style.display = visible ? "none" : "block";
+        }
+    </script>
+</head>
+<body>
+    <div class="dashboard">
+        <!-- Sidebar -->
+        <aside class="sidebar">
+            <div>
+                <h2>Panel Admin</h2>
+                <nav>
+                    <ul>
+                        <!-- Opción Inicio -->
+                        <li>
+    <a href="{{ route('dashboard') }}" class="btnInicio"><span>Inicio</span></a>
+</li>
+
+
+                        <li onclick="toggleSubmenu(this)">
+                            <span>Usuarios</span>
+                            <ul class="submenu">
+                                <li><a href="{{ route('usuarios.create') }}">Registrar Usuario</a></li>
+                                <li><a href="{{ route('usuarios.index') }}">Consultar Usuarios</a></li>
+                            </ul>
+                        </li>
+                        <li onclick="toggleSubmenu(this)">
+                            <span>Minutas</span>
+                            <ul class="submenu">
+                                <li><a href="{{ route('minutas.create') }}">Registrar Minuta</a></li>
+                                <li><a href="{{ route('minutas.index') }}">Consultar Minutas</a></li>
+                                <li><a href="{{ route('minutas.historial') }}" >Ver Historial</a></li>
+                            </ul>
+                        </li>
+                        <li onclick="toggleSubmenu(this)">
+                            <span>Recursos</span>
+                            <ul class="submenu">
+                                <li><a href=" {{ route('recursos.create') }}">Registrar Recurso</a></li>
+                                <li><a href="{{  route('recursos.index') }}">Consultar Recursos</a></li>
+                            </ul>
+                        </li>
+                        <li onclick="toggleSubmenu(this)">
+                            <span>Traslado Recursos</span>
+                            <ul class="submenu">
+                                <li><a href=" {{ route('traslados.create') }}">Registrar Traslado</a></li>
+                                <li><a href="{{  route('traslados.index') }}">Consultar Traslado</a></li>
+                            </ul>
+                        </li>
+                        <li onclick="toggleSubmenu(this)">
+                            <span>Incidentes</span>
+                            <ul class="submenu">
+                                <li><a href="{{ route('incidentes.create') }}">Registrar Incidente</a></li>
+                                <li><a href="{{ route('incidentes.index') }}">Consultar Incidente</a></li>
+                                <li><a href="{{ route('historico_incidentes.index') }}">Histórico Incidentes</a></li>
+                            </ul>
+                        </li>
+                        <li onclick="toggleSubmenu(this)">
+                            <span>Asistencias</span>
+                            <ul class="submenu">
+                                <li><a href="{{ route('registro_inasistencia.create') }}">Registrar Asistencia</a></li>
+                                <li><a href="{{ route('registro_inasistencia.index') }}">Consultar Asistencia</a></li>
+                            </ul>
+                        </li>
+                        <li onclick="toggleSubmenu(this)">
+                            <span>Ambientes</span>
+                            <ul class="submenu">
+                                <li><a href="{{ route('ambientes.create') }}">Registrar Ambiente</a></li>
+                                <li><a href="{{ route('ambientes.index') }}">Consultar Ambiente</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+            <!-- Botón cerrar sesión -->
+            <form action="{{ route('logout') }}" method="POST" style="display:inline;">
+    @csrf
+    <button type="submit" class="btn-logout">🚪 Cerrar sesión</button>
+</form>
+
+        </aside>
+
+        <!-- Contenido dinámico -->
+        <main class="content">
+           @if(Auth::check())
+    <p>Bienvenido {{ Auth::user()->p_nombre }}  {{ Auth::user()->p_apellido }}</p>
+@else
+    <script>window.location.href = "{{ route('login') }}";</script>
+@endif
+
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
