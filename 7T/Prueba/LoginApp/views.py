fb02b112b4e0cf96eae7943e294784283c724d4a
Kennencortes <kennencortes123@gmail.com>
2026-03-17 19:42:50 -0500

    docs: se termina de agregar el formulario

--- 7T/Prueba/LoginApp/views.py
+++ 7T/Prueba/LoginApp/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render, redirect
-#from django.views.generic import View
-#from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-#from django.contrib.auth import login, logout, authenticate
-#from django.contrib import messages
-# Create your views here.
+from django.views.generic import View
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login, logout, authenticate
+from django.contrib import messages
+from .forms import FormLogin
 
-def login(request):
-  return render(request, 'login.html')
- #if request.method == 'POST':
-  #      doc = request.POST.get('doc')
-   #     password = request.POST.get('pass')
-#
- #       user = authenticate(request, username=doc, password=password)
-#
- #       if user is not None:
-  #          login(request, user)
-   #         return redirect('index')
-    #  else:
-     #       messages.error(request, 'Credenciales incorrectas')
-      #      
-       
+
+def login_view(request):
+
+    if request.method == 'POST':
+        miForm = FormLogin(request.POST)
+
+        if miForm.is_valid():
+            datosForm = miForm.cleaned_data
+
+            user = authenticate(
+                request,
+                username=datosForm["Cedula"],
+                password=datosForm["contraseña"]
+            )
+
+            if user is not None:
+                auth_login(request, user)
+                return redirect('index')
+            else:
+                messages.error(request, "Credenciales incorrectas")
+    else:
+        miForm = FormLogin()
+
+    return render(request, "login.html", {"miForm": miForm})
