bc18425794d5c25c4f990a0b7b55778b91c2f519
Luchoj06 <soylucho2006@gmail.com>
2026-04-03 13:04:33 -0500

    docs: se agregan los modelos a la app de instructor

--- 7T/Prueba/InstApp/models.py
+++ 7T/Prueba/InstApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from UsuarioApp.models import Usuario
+from CoordinacionApp.models import Coordinacion
 
-# Create your models here.
+class Instructor(models.Model):
+    usuario = models.OneToOneField(
+        Usuario,
+        on_delete=models.DO_NOTHING,
+        db_column='Usuario_id_usuario',
+        primary_key=True
+    )
+    email = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
+    coordinacion = models.ForeignKey(
+        Coordinacion,
+        on_delete=models.DO_NOTHING,
+        db_column='coordinacion_id_coordinacion'
+    )
+    estado = models.CharField(max_length=8)
+
+    class Meta:
+        managed = False
+        db_table = 'instructor'
