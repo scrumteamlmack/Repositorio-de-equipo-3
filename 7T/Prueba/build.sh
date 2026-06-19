4bdb04604b5ce16a7e478cc75d58e44234f2fb90
Mahilynwn <mahilyg17@gmail.com>
2026-06-18 22:27:54 -0500

    Add create_temp_table helper script and update build.sh

--- 7T/Prueba/build.sh
+++ 7T/Prueba/build.sh
@@ -8,5 +8,8 @@ pip install -r requirements.txt
 # Compile static files
 python manage.py collectstatic --no-input
 
+# Create helper temporary table if missing to prevent migration crash
+python create_temp_table.py
+
 # Run migrations
 python manage.py migrate
